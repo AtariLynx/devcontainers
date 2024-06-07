74b906d4aec37a8a167996a92bc84e1d0e05f4e2
Marcin Słowikowski <marcin.slowikowski@xebia.com>
2024-06-07 10:52:28 +0200

    Fixing tests

--- test/cc65/test.sh
+++ test/cc65/test.sh
@@ -3,7 +3,7 @@ cd $(dirname "$0")
 source test-utils.sh
 
 # Template specific tests
-check "cc65" [ $(cc65 | grep No input files) ]
+check "cc65" [ $(cc65 | grep "No input files") ]
 
 # Report result
 reportResults
