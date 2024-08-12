e39b9d6ca60652ec6ba6d048f4276224fb86bbf1
Alex Thissen <athissen@killer-apps.nl>
2024-08-12 13:01:27 +0000

    Include Atari Lynx features by default in cc65 image Improvements to test for Atari Lynx template

--- test/cc65/test.sh
+++ test/cc65/test.sh
@@ -4,6 +4,8 @@ source test-utils.sh
 
 # Template specific tests
 check "cc65" [ "$(cc65 2>&1 | grep 'No input files')" ]
+check "sprpck" [ "$(sprpck | grep 'Lynx Sprite Packer Ver 2.4')" ]
+checl "make_lnx" [ "$(make_lnx | grep 'Raw image to LNX image convertor V5')" ]
 
 # Report result
 reportResults
