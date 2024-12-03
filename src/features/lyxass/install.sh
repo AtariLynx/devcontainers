a47d7308f8526654b2b31b62238e0fc31339ff0a
Alex Thissen <athissen@killer-apps.nl>
2024-12-03 19:35:09 +0100

    Added features for new_bll and lyxass

---
+++ src/features/lyxass/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'make_lnx'"
+SOURCE_DIR=/usr/src
+mkdir -p ${SOURCE_DIR}
+cd ${SOURCE_DIR}
+git clone https://github.com/42Bastian/lyxass.git
+cd lyxass
+make
+install lyxass /usr/bin
+
+rm -rf ${SOURCE_DIR}/lyxass
+
+# cd ${SOURCE_DIR}
+# git clone https://github.com/42Bastian/new_bll.git
+# cd new_bll/lynxenc
+# make
+# install lynxenc lynxdec /usr/bin
+# cd ../make_lnx
+# make
+# install make_lnx /usr/bin
