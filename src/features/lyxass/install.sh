e63617a776fa1e29a88ebd251889136ee14b6a76
Alex Thissen <athissen@killer-apps.nl>
2024-12-19 14:29:13 +0100

    Removed unnecessary code from lyxass install script

--- src/features/lyxass/install.sh
+++ src/features/lyxass/install.sh
@@ -11,12 +11,3 @@ make
 install lyxass /usr/bin
 
 rm -rf ${SOURCE_DIR}/lyxass
-
-# cd ${SOURCE_DIR}
-# git clone https://github.com/42Bastian/new_bll.git
-# cd new_bll/lynxenc
-# make
-# install lynxenc lynxdec /usr/bin
-# cd ../make_lnx
-# make
-# install make_lnx /usr/bin
